6a1cc5d44f7db43ff4413980f45f32180672f10b
Armaan5a <bharad55@gmail.com>
2021-08-06 18:57:10 +0530

    smoak added

--- Bird.js
+++ Bird.js
@@ -1,12 +1,20 @@
 class Bird extends BaseClass {
   constructor(x,y){
-    super(x,y,50,50);
-    this.image = loadImage("sprites/bird.png");
+    super(x,y,70,70);
+    this.image = loadImage("sprites/yellow.png");
+    this.im= loadImage("sprites/smoke.png");
+    this.array=[]
   }
 
   display() {
-    //this.body.position.x = mouseX;
-    //this.body.position.y = mouseY;
     super.display();
+  
+     if(this.body.velocity.x>1.5 && this.body.position.x>220){
+    var pos =[this.body.position.x,this.body.position.y]
+     this.array.push(pos)
+     }
+     for(var i=0;i<this.array.length;i=i+1){
+       image(this.im,this.array[i][0],this.array[i][1])
+     }
   }
 }
